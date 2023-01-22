5f65115044f85f11d6c0ba1092f55e320692c7b9
itzaur <preneur85@gmail.com>
2023-01-22 22:26:29 +0300

    add babel to project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
+module.exports = {
+  mode: mode,
+
+  devtool: "source-map",
+
+  devServer: {
+    static: "./dist",
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
