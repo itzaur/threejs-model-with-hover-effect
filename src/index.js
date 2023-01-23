fccb0bce3a8c7b8cbec4b3fba00d6cad910a3b46
itzaur <preneur85@gmail.com>
2023-01-23 23:54:51 +0300

    add hover and rotation effects

--- src/index.js
+++ src/index.js
@@ -33,6 +33,11 @@ export default class Sketch {
       new THREE.Color(color4),
     ];
 
+    //Hover parameter
+    this.uniforms = {
+      uHover: 0,
+    };
+
     this.resize = () => this.onResize();
     this.mousemove = (e) => this.onMousemove(e);
   }
@@ -43,6 +48,7 @@ export default class Sketch {
     this.createCamera();
     this.createRenderer();
     this.createLoader();
+    this.createRaycaster();
 
     this.loadModel().then(() => {
       this.renderer.setAnimationLoop(() => {
@@ -87,20 +93,24 @@ export default class Sketch {
   loadModel() {
     return new Promise((resolve) => {
       this.gltfLoader.load("models/brain.glb", (gltf) => {
-        const brain = gltf.scene.children[0];
+        this.brain = gltf.scene.children[0];
         // this.scene.add(brain);
 
-        // this.geometry = new THREE.ConeGeometry(0.005, 0.01, 10);
-        this.geometry = new THREE.IcosahedronGeometry(0.005, 1);
+        this.geometry = new THREE.ConeGeometry(0.003, 0.005, 10);
+        // this.geometry = new THREE.IcosahedronGeometry(0.005, 1);
         this.material = new THREE.ShaderMaterial({
           wireframe: true,
           vertexShader: require("./static/shaders/brain.vertex.glsl"),
           fragmentShader: require("./static/shaders/brain.fragment.glsl"),
           uniforms: {
             uColor: { value: new THREE.Color() },
+            uPointer: { value: new THREE.Vector3() },
+            uSize: { value: 0 },
+            uRotation: { value: 0 },
+            uHover: { value: this.uniforms.uHover },
           },
         });
-        this.count = brain.geometry.attributes.position.count;
+        this.count = this.brain.geometry.attributes.position.count;
 
         this.instancedMesh = new InstancedUniformsMesh(
           this.geometry,
@@ -112,7 +122,7 @@ export default class Sketch {
 
         const dummy = new THREE.Object3D();
 
-        const positions = brain.geometry.attributes.position.array;
+        const positions = this.brain.geometry.attributes.position.array;
         for (let i = 0; i < positions.length; i += 3) {
           dummy.position.set(
             positions[i + 0],
@@ -123,6 +133,17 @@ export default class Sketch {
           dummy.updateMatrix();
 
           this.instancedMesh.setMatrixAt(i / 3, dummy.matrix);
+
+          this.instancedMesh.setUniformAt(
+            "uSize",
+            i / 3,
+            THREE.MathUtils.randFloat(3, 0.3)
+          );
+          this.instancedMesh.setUniformAt(
+            "uRotation",
+            i / 3,
+            THREE.MathUtils.randFloat(1, -1)
+          );
 
           const colorIndex = THREE.MathUtils.randInt(0, this.colors.length - 1);
           this.instancedMesh.setUniformAt(
@@ -166,6 +187,26 @@ export default class Sketch {
     this.checkMobile();
   }
 
+  createRaycaster() {
+    this.mouse = new THREE.Vector2();
+    this.point = new THREE.Vector3();
+
+    this.raycaster = new THREE.Raycaster();
+    this.intersects = [];
+  }
+
+  animateUniformsHover(value) {
+    gsap.to(this.uniforms, {
+      uHover: value,
+      duration: 0.3,
+      onUpdate: () => {
+        for (let i = 0; i < this.instancedMesh.count; i++) {
+          this.instancedMesh.setUniformAt("uHover", i, this.uniforms.uHover);
+        }
+      },
+    });
+  }
+
   onMousemove(e) {
     const x = (e.clientX / this.width) * 2 - 1;
     const y = -((e.clientY / this.height) * 2 - 1);
@@ -175,6 +216,30 @@ export default class Sketch {
       y: () => y * 0.1,
       duration: 0.5,
     });
+
+    this.mouse.set(x, y);
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    this.intersects = this.raycaster.intersectObject(this.brain);
+
+    if (this.intersects.length === 0) {
+      //   console.log("mouseleave");
+      this.animateUniformsHover(0);
+    } else {
+      //   console.log("mouseenter");
+      this.animateUniformsHover(1);
+
+      gsap.to(this.point, {
+        x: () => this.intersects[0]?.point.x,
+        y: () => this.intersects[0]?.point.y,
+        z: () => this.intersects[0]?.point.z,
+        duration: 0.3,
+        onUpdate: () => {
+          for (let i = 0; i < this.instancedMesh.count; i++) {
+            this.instancedMesh.setUniformAt("uPointer", i, this.point);
+          }
+        },
+      });
+    }
   }
 
   addListeners() {
--- src/static/shaders/brain.vertex.glsl
+++ src/static/shaders/brain.vertex.glsl
@@ -1,6 +1,29 @@
-void main() {
-    vec4 mvModel = vec4(position, 1.0);
-    mvModel = instanceMatrix * mvModel;
+uniform float uSize;
+uniform float uRotation;
+uniform float uHover;
+uniform vec3 uPointer;
 
-    gl_Position = projectionMatrix * modelViewMatrix * mvModel;
+#define PI 3.14159265359
+
+mat2 rotate2d(float _angle){
+    return mat2(cos(_angle),-sin(_angle),
+                sin(_angle),cos(_angle));
+}
+
+void main() {
+    vec4 mvPosition = vec4(position, 1.0);
+    mvPosition = instanceMatrix * mvPosition;
+
+    float distance = distance(uPointer, mvPosition.xyz);
+    float color = smoothstep(0.45, 0.1, distance);
+
+    float scale = uSize + color * 5.*uHover;
+
+    vec3 pos = position;
+    pos *= scale;
+    pos.xz *= rotate2d(color * uRotation * PI + uRotation * PI * 0.4);
+    pos.xy *= rotate2d(color * uRotation * PI + uRotation * PI * 0.7);
+    mvPosition = instanceMatrix * vec4(pos, 1.0);
+
+    gl_Position = projectionMatrix * modelViewMatrix * mvPosition;
 }
